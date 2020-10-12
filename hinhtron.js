565ec7deb5c5fba283c1cd296c5fd03fb89e4132
quynhbuitruc <quynhbui270492@gmail.com>
2020-10-12 16:39:00 +0900

    sửa prompt thành function

--- hinhtron.js
+++ hinhtron.js
@@ -1,3 +1,12 @@
-let bankinh = prompt("Bán kính hình tròn là:");
-dienTich = bankinh * bankinh * Math.PI;
-alert("Diện tích hình tròn là:" + dienTich)
+
+function chuvi() {
+    let bankinh = Number(document.getElementById("bankinh").value);
+    let chuVi = 2 * bankinh * Math.PI;
+    document.getElementById("chuVi").innerHTML = chuVi;
+}
+
+function dienTich() {
+    let bankinh = Number(document.getElementById("bankinh").value);
+    let dienTich = bankinh * bankinh * Math.PI;
+    document.getElementById("dienTich2").innerHTML = dienTich;
+}
